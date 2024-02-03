5f80b299a0664d3bb307c873e07df45dd3eb05b9
Scott Twede <stwede@gmail.com>
2024-02-03 13:00:30 -0800

    add debug interface

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-import './style.css'
+import gsap from 'gsap';
+import GUI from 'lil-gui';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import './style.css';
+
+const gui = new GUI().close();
+const cubeTweaks = gui.addFolder('Cube Tweaks');
+const debug = {};
 
 const scene = new THREE.Scene();
 
@@ -15,10 +21,49 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: false });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+debug.color = '#8f0';
+debug.subdivision = 2;
+
+const geometry = new THREE.BoxGeometry(1, 1, 1, debug.subdivision, debug.subdivision, debug.subdivision);
+const material = new THREE.MeshBasicMaterial({
+  color: debug.color,
+  wireframe: true
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+cubeTweaks
+  .add(mesh.position, 'y')
+  .min(-3)
+  .max(3)
+  .step(0.1)
+  .name('Elevation');
+
+cubeTweaks
+  .add(mesh, 'visible');
+
+cubeTweaks
+  .add(material, 'wireframe');
+
+cubeTweaks
+  .addColor(debug, 'color')
+  .onChange(() => {
+    material.color.set(debug.color);
+  });
+
+cubeTweaks
+  .add(debug, 'subdivision')
+  .min(1)
+  .max(20)
+  .step(1)
+  .onFinishChange(() => {
+    mesh.geometry.dispose();
+    mesh.geometry = new THREE.BoxGeometry(1, 1, 1, debug.subdivision, debug.subdivision, debug.subdivision);
+  });
+
+debug.rotateY = () => gsap.to(mesh.rotation, { y: mesh.rotation.y + Math.PI * 2 });
+cubeTweaks
+  .add(debug, 'rotateY');
 
 camera.position.z = 3;
 
@@ -40,8 +85,8 @@ window.addEventListener('dblclick', () => {
 function animate () {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.01;
 
   controls.update();
 
